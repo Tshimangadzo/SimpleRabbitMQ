ee1bc0729de8a9ae6c3c02b7c16fe1d7cf721f58
Tshimangadzo <mudzanani27@gmail.com>
2023-05-10 10:16:23 +0200

    added consumers and producer

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new()
+{
+    Uri = new Uri("amqp://rabbituser:password@localhost:5672"),
+    ClientProvidedName = "Producer"
+};
+
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string exchangeName = "exchange-name";
+string routingKey = "routing-key";
+string queueName = "queue-name";
+string messageToSend = "Message #";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, false, false,null);
+channel.QueueDeclare(queueName, false, false, false, null);
+
+channel.QueueBind(queueName,exchangeName,routingKey,null);
+
+for (int i = 0; i < 250;i++) {
+    byte[] bytes = Encoding.UTF8.GetBytes(messageToSend+$"{i}");
+    channel.BasicPublish(exchangeName, routingKey, null, bytes);
+    Console.WriteLine("Message sent");
+}
---
+++ Receiver2/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new()
+{
+    Uri = new Uri("amqp://rabbituser:password@localhost:5672"),
+    ClientProvidedName = "Consumer 2"
+};
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+string exchangeName = "exchange-name";
+string routingKey = "routing-key";
+string queueName = "queue-name";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, false, false, null);
+channel.QueueDeclare(queueName, false,false,false,null);
+
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (sender, e) =>
+{
+    Thread.Sleep(3000);
+    var body = e.Body.ToArray();
+    string bodyMessage = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Consumer 2: " + bodyMessage);
+    channel.BasicAck(e.DeliveryTag,false);
+    
+};
+
+string tag = channel.BasicConsume(queueName, false,consumer);
+
+Console.ReadLine();
+channel.BasicCancel(tag);
+
+channel.Close();
+connection.Close();
